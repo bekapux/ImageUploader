45f757113bd58794487cb1a83885a8cf56e40796
Beka Pukhashvili <beka.pukhashvili@gmail.com>
2022-04-13 22:01:01 +0400

    initial commit

---
+++ ImageUploader/Controllers/ImagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageUploader.API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ImagesController : ControllerBase
+  {
+    [HttpPost]
+    public async Task<IActionResult> Upload(IFormFile file)
+    {
+      if (file == null || file.Length == 0) return BadRequest();
+      return Ok();
+    }
+  }
+}
